fbad901c25f1c9001d89658f6cce01210df0cf4a
Tim Allen <tallen@integsys.biz>
2020-07-24 00:37:11 -0400

    it works! (I get a beige screen)

--- vga.sv
+++ vga.sv
@@ -1,24 +1,35 @@
 module vga (
 	input CLOCK_50,
 	output reg [7:0]VGA_B,
-	output bit VGA_BLANK_N, // redundant if RG&B are 0?
+	output bit VGA_BLANK_N, // to D2A chip, active low
 	output bit VGA_CLK, // latch the RGBs and put 'em on the DACs
 	output reg [7:0]VGA_G,
-	output bit VGA_HS,
+	output bit VGA_HS, // DB19 pin, active low
 	output reg [7:0]VGA_R,
-	output bit VGA_SYNC_N, //sync on green???
-	output bit VGA_VS);
+	output bit VGA_SYNC_N, // to D2A chip, active low
+	output bit VGA_VS); // DB19 pin, active low
 
 logic [6:0] counter_10;
 logic [8:0] h_counter;
 logic [9:0] v_counter;
-logic [23:0] framebuffer [200*600-1:0]; //200 x 600
+//logic [23:0] fb_d;
+//logic [16:0] fb_adr;
+//logic [23:0] fb_q;
+//logic fb_rw;
+
+//assign fb_adr = h_counter + v_counter * 17'd200;
+
+//single_port_ram #(24, 8) framebuffer (fb_d, fb_adr, fb_rw, VGA_CLK, fb_q);
 
 initial begin
 	counter_10 = 0;
 	h_counter = 0;
 	v_counter = 0;
-	$readmemh("screenshot.txt", framebuffer);
+//	fb_rw = 1'd1;
+	VGA_HS = 1'd1;
+	VGA_VS = 1'd1;
+	VGA_SYNC_N = 1'd1;
+	VGA_BLANK_N = 1'd1;
 	end
 
 always @ (posedge CLOCK_50) begin
@@ -39,19 +50,20 @@ always @ (posedge VGA_CLK) begin
 	end
 	if (h_counter == 210) begin
 		//hfront porch end, hsync pluse start
-		VGA_HS = 1; //hsync start
-		VGA_SYNC_N = 1; //not sure about this
+		VGA_HS = 0; //hsync start
+		VGA_SYNC_N = 0; //not sure about this
 	end
 	if (h_counter == 242) begin
 		//hsync pulse stop, hback porch start
-		VGA_HS = 0; //hsync end
-		VGA_SYNC_N = 0; //not sure about this
+		VGA_HS = 1; //hsync end
+		VGA_SYNC_N = 1; //not sure about this
 	end
 	if (h_counter == 264) begin
 		//hback porch end
 		h_counter = 0;
 		v_counter++;
-		VGA_BLANK_N = 1; //re-enable RG&B ?
+		if (v_counter <= 600) //are we in vertical blanking?
+			VGA_BLANK_N = 1; //re-enable RG&B ?
 	end
 	if (v_counter == 600) begin
 		//front porch start
@@ -59,19 +71,19 @@ always @ (posedge VGA_CLK) begin
 	end
 	if (v_counter == 601) begin
 		//vfront porch end, vsync pulse start
-		VGA_VS = 1; //vsync start
+		VGA_VS = 0; //vsync start
 	end
 	if (v_counter == 605) begin
 		//vsync pulse end, vback porch start
-		VGA_VS = 0; //vsync end
+		VGA_VS = 1; //vsync end
 	end
 	if (v_counter == 628) begin
 		//vback porch end
 		v_counter = 0;
 		VGA_BLANK_N = 1; //re-enable RG&B ?
 	end
-	VGA_R = framebuffer[h_counter+v_counter*200][23:16];
-	VGA_G = framebuffer[h_counter+v_counter*200][15:8];
-	VGA_B = framebuffer[h_counter+v_counter*200][7:0];
+	VGA_R = 8'ha0; //fb_d[23:16];
+	VGA_G = 8'h20; //fb_d[15:8];
+	VGA_B = 8'h40; //fb_d[7:0];
 end
 endmodule
