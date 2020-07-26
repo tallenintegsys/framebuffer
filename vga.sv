79174e5cae154d6ebb662e626760f3f54d13e04d
Tim Allen <tallen@integsys.biz>
2020-07-26 15:31:05 -0400

    more simplification

--- vga.sv
+++ vga.sv
@@ -34,9 +34,8 @@ initial reset_counter = 0;
 
 always @ (posedge CLOCK_50) begin
 	reset_counter++;
-	if (reset_counter == 5) begin
+	if (reset_counter == 5)
 		reset <= 1;
-	end
 	VGA_CLK <= ~VGA_CLK; //25MHz
 end
 
@@ -57,29 +56,23 @@ end else begin
 	000: h_blank <= 0; //blank
 	039: h_blank <= 1; //unblank
 	599: h_blank <= 0; //blank
-	639: begin //hfront porch start
-		h_blank <= 0; //disable RGB DACs
-	end
+	639: h_blank <= 0; //hfront porch start
 	655: begin //hfront porch end
 		VGA_HS <= 0; //hsync start
 		v_advance <= ~v_advance;
-		if (!v_counter[0] && (v_counter >= 48) && (v_counter < 480)) begin
-			fb_adr <= fb_adr - 280;
-		end
+		if (!v_counter[0] && v_blank)
+			fb_adr <= fb_adr - 280; //redo the line (line double)
 	end
-	751: begin //hback porch start
-		VGA_HS <= 1'd1; //hsync end
-	end
+	751: VGA_HS <= 1'd1; //hback porch start
 	800: begin //hback porch end
 		h_counter <= 0;
 		v_counter++;
 	end
 	endcase
-	if ((h_blank & v_blank)) begin
-		if (!h_counter[0]) begin
+
+	if ((h_blank & v_blank))
+		if (!h_counter[0])
 			fb_adr <= fb_adr + 1;
-		end
-	end
 
 	case (v_counter)
 	000: v_blank <= 0; //blank
@@ -89,18 +82,15 @@ end else begin
 		v_blank <= 0; //disable RGB DACs
 		fb_adr <= 0;
 	end
-	491: begin //vfront porch end
-		VGA_VS <= 0; //vsync start
-	end
-	493: begin //vsync pulse end
-		VGA_VS <= 1'd1; //vsync end
-	end
+	491: VGA_VS <= 0; //vfront porch end//vsync start
+	493: VGA_VS <= 1'd1; //vsync pulse end//vsync end
 	524: begin //vback porch end
 		v_blank <= 1'd1; //enable RGB DACs
 		v_counter <= 0;
 		v_advance <= 0;
 	end
 	endcase
+
 	VGA_R <= fb_q[23:16];
 	VGA_G <= fb_q[15:8];
 	VGA_B <= fb_q[7:0];
